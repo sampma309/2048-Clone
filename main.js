59b484a6e1fb76619348f16a5acd0dc477fcd172
Michael Samp <michael.samp94@gmail.com>
2022-07-26 18:01:31 -0500

    added new game button and a function to generate a new board

--- main.js
+++ main.js
@@ -1,49 +1,36 @@
 const gameGrid = {
-    nums: [[1, 2, 3, 4],
-           [5, 6, 7, 8],
-           [9, 10, 11, 12],
-           [13, 14, 15, 16]],
+    nums: [[0, 0, 0, 0],
+           [0, 0, 0, 0],
+           [0, 0, 0, 0],
+           [0, 0, 0, 0]],
 
-    /*
-    generateBoard: function () {
-        let nums = 0;
-        do {
-            let index = Math.floor(Math.random() * 16) + 1;
-            let num = 0
-            if (Math.random() < 0.5) {
-                num = 2;
-            } else {
-                num = 4;
+    generateNewBoard: function () {
+        
+        // Reset nums array
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                this.nums[i][j] = 0;
             }
-            let boxNumber = document.getElementById("box" + index).innerHTML;
-            if (!boxNumber) {
-                document.getElementById("box" + index).innerHTML = num;
-                nums += 1;
-            }
-        } while (nums < 3);
-        // add call to display board function below and instead just change the nums attribute
-    },*/
+        }
 
-    generateBoard: function () {
-        /*let numsGenerated = 0;
-        do {
+        // Place two numbers randomly (50% chance to be 2 or 4) 
+        let numsGenerated = 0;
+        while (numsGenerated < 2) {
             let index = Math.floor(Math.random() * 16);
             let num = 0
-            if (Math.random() < 0.5) {
+            if (Math.random() < 0.66) {
                 num = 2;
             } else {
                 num = 4;
             }
-            let boxNumber = this.nums[Math.floor(index / 4)][index % 4];
-            if (boxNumber !== 0) {
+            if (this.nums[Math.floor(index / 4)][index % 4] === 0) {
                 this.nums[Math.floor(index / 4)][index % 4] = num
                 numsGenerated += 1;
             }
-            console.log(index);
-            console.log(num);
-        } while (numsGenerated < 3);*/
+        }
+
+        // Display board
         this.displayBoard();
-        console.log(this.nums[1][1]);
     },
 
     moveTiles: function (e) {
@@ -75,7 +62,6 @@ const gameGrid = {
     displayBoard: function () {
         for (let i = 0; i < 4; i++) {
             for (let j = 0; j < 4; j++) {
-                console.log("box" + (i * 4 + j + 1));
                 document.getElementById("box" + (i * 4 + j + 1)).innerHTML = this.nums[i][j];
             }
         }
